cc1437841c66646edbb5016203eae16307c00baa
Nastia Gusev <gusev.nastia@gmail.com>
2022-07-15 14:41:27 +0300

    Initial Commit

---
+++ MyToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDo
+//
+//  Created by Nastia Gusev on 15/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
