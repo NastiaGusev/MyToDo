4e85475d3f36d1591414a1a5d0084e6c6b9233b8
Nastia Gusev <gusev.nastia@gmail.com>
2022-07-16 16:47:49 +0300

    Added TableView and User Defaults handling for saving data

--- MyToDo/AppDelegate.swift
+++ MyToDo/AppDelegate.swift
@@ -11,13 +11,36 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Gets called when the app is loaded up
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    func applicationWillResignActive(_ application: UIApplication) {
+        //Sent when the application is about to move from active to inactive state.
+        //Temporary interactions - phone call/SMS, user quits the application and it
+        //begins to transition to background state
+        
+        //Use this method to pause ongoing tasks
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        //When the application disapears from the screen - pressing the home button, open a different app.
+        //Use this method to release shared resources, save users data, invalidate timers, store application state
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        //
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        //
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        //The app is going to be terminated
+        //If the user force quit the app, if there is another app using a lot of resources
+    }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -33,4 +56,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
